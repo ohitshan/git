fd5b0fa2347320a1fd8fc00c0af1d6b591835cb6
han <rkddkwl1364@gmail.com>
2022-07-17 20:16:21 +0900

    new commit

--- pages/hello.tsx
+++ pages/hello.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function hello() {
-  return <div>helloiam the main all of branches are deleted</div>;
+  return <div>helloiam the main all of branches are deleted new commit</div>;
 }
 
 export default hello;
